90f547a2f346b7f083b49742b35d5d3dce205e17
AdrianMB24 <51972772+AdrianMB24@users.noreply.github.com>
2020-10-16 22:18:41 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setup(
     author='Carlos_Joseph_Adrian_Emmanuel', 
     packages=['Metodos'],
     python_requires='>=3',
-    install_requires=['tabulate', 'playsound', 'cv2', 'numpy', 'PIL', 'sys', 'argparse', 'time'],
+    install_requires=['tabulate', 'playsound', 'opencv-constrib-python', 'pillow', 'argparse' ],
     entry_points={
         'console_scripts': [
            'Tarea3=Tarea3:Main',
